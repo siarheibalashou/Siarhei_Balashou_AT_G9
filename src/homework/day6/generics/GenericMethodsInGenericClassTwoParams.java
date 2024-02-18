a2b48d27aaf044fa0545bd8ff8d4bdc6393fe68b
Siarhei Balashou <siarhei.balashou@yandex.by>
2024-02-18 17:01:13 +0300

    added runner class

--- src/homework/day6/generics/GenericMethodsInGenericClassTwoParams.java
+++ src/homework/day6/generics/GenericMethodsInGenericClassTwoParams.java
@@ -11,12 +11,13 @@ public class GenericMethodsInGenericClassTwoParams<X, Y> {
         return "I received  1 arguments of type: " + firstArg.getClass().getSimpleName() + " class ";
     }
 
-    public void genericMethodHalfGenArgs(X firstArg, String secondArg){
-
+    public void genericMethodHalfGenArgs(X firstArg, String secondArg) {
+        int stringLength = secondArg.length();
+        System.out.println("I got an object of " + firstArg.getClass().getSimpleName() + " class and string with " + stringLength + " characters");
     }
 
-    public void genericMethodHalfGenArgs(X firstArg,Y secondArg, String thirdArg){
+    public void genericMethodHalfGenArgs(X firstArg, Y secondArg, String thirdArg) {
         int stringLength = thirdArg.length();
-        System.out.println("I got an object of X class and Y class and string with "+stringLength+ " characters");
+        System.out.println("I got an object of " + firstArg.getClass().getSimpleName() + " class and " + secondArg.getClass().getSimpleName() + " class and string with " + stringLength + " characters");
     }
 }
---
+++ src/homework/day6/generics/GenericMethodsInGenericClassTwoParamsRunner.java
@@ -0,0 +1,10 @@
+package homework.day6.generics;
+
+public class GenericMethodsInGenericClassTwoParamsRunner {
+    public static void main(String[] args) {
+        GenericMethodsInGenericClassTwoParams<String,String> genericMethodsInGenericClassTwoParamsStrStr = new GenericMethodsInGenericClassTwoParams<>();
+        GenericMethodsInGenericClassTwoParams<String,Integer> genericMethodsInGenericClassTwoParamsStrInteger = new GenericMethodsInGenericClassTwoParams<>();
+        GenericMethodsInGenericClassTwoParams<String,Double> genericMethodsInGenericClassTwoParamsStrDouble = new GenericMethodsInGenericClassTwoParams<>();
+
+    }
+}
