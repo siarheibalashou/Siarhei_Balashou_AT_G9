7dd392795bdd0076e2220e338239bf18336d5c7e
Siarhei Balashou <siarhei.balashou@yandex.by>
2024-03-10 00:52:13 +0300

    classwork day 11

---
+++ src/classwork/day11/AnonimousMethod.java
@@ -0,0 +1,12 @@
+package classwork.day11;
+
+public class AnonimousMethod {
+    {
+        int four=2*2;
+        System.out.println(four);
+    }
+    static {
+        int five = 3+2;
+        System.out.println(five);
+    }
+}
